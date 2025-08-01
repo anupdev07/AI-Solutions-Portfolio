796ab23547caec0fce22901ba4dffcc1c009c2d7
anupdev07 <devp.anup@gmail.com>
2025-08-01 08:41:33 +0545

    second commit

--- server/app.js
+++ server/app.js
@@ -2,17 +2,5 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-mongoose.connection.on('error', (err) => {
-  console.error('MongoDB connection error:', err);
-});
-mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB');
-});
